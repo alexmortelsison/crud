3691df6b49b86f6c98671b9b695fcda3e1ff1455
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-06-06 08:21:59 +0900

    added firebase

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,24 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Theme.of(context).colorScheme.tertiary,
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        title: Center(
+          child: Text(
+            'Notes',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+        ),
+      ),
       backgroundColor: Theme.of(context).colorScheme.surface,
     );
   }
